26dd912f624325e06d0ecf81684c9028ed309bcc
Cassiel Rattes Cortez <cassielrattes@gmail.com>
2022-07-21 00:02:55 -0300

    Create a CLI

---
+++ LifeCycle/this-apply-arguments/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { watch, promises: { readFile } } = require('fs');
+
+class File {
+    watch(event, filename) {
+        console.log('this', this);
+        console.log('arguments', Array.prototype.slice.call(arguments));
+
+        this.showContent(filename);
+    }
+
+    async showContent() {
+        const contentFileBuffer = await readFile(filename);
+        const contentFileString = contentFileBuffer.toString();
+
+        console.log(contentFileString);
+    }
+}
+
+const file = new File();
+
+// Ignora o this da classe File e herda o this do watch
+// watch(__filename, file.watch)
+
+// alternativa para nao herdar o this da funcao (NAO É A MELHOR ALTERNATIVA)
+// watch(__filename, (event, filename) => file.watch(event, filename))
+
+// Podemos deixar explicito qual o contexto que a funcao deve seguir
+// O bind retorna uma funcao com o this que se mantem de file, ignorando o do watch
+// watch(__filename, file.watch.bind(file))
+
+// a diferenca entre um e outro é que o .call passa uma lista de argumentos e o .apply passa os argumentos como array
+file.watch.call({ showContent: () => console.log('call: hello showContent!') }, null, __filename);
+file.watch.apply({ showContent: () => console.log('apply: hello showContent!') }, [null, __filename]);
